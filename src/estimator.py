1fc1a2fb56fe28e4de56b336442c6af27face579
magdelineM <40407512+magdelineM@users.noreply.github.com>
2020-04-11 13:36:29 +0200

    Update estimator.py

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,20 @@
 def estimator(data):
+  {
+region: {
+name: "Africa",
+avgAge: 19.7,
+avgDailyIncomeInUSD: 5,
+avgDailyIncomePopulation: 0.71
+},
+periodType: "days",
+timeToElapse: 58,
+reportedCases: 674,
+population: 66622705,
+totalHospitalBeds: 1380614
+}
   return data
+{
+data: {}, 
+impact: {},
+severeImpact: {}
+}
